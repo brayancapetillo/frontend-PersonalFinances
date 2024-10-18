2ffa34abe0ad1994a6d62eafa193090466d256aa
Brayan Capetillo <brayancapetoccc@gmail.com>
2024-10-18 01:38:27 -0600

    :sparkles: feat(component): update `login` component
    Updated the login component with the following changes:
    - Added structure for the component
    - Included styles for improved usability and aesthetics

--- src/app/content/auth/login/login.component.ts
+++ src/app/content/auth/login/login.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core'
+import { Engine, ParticlesOptions, RecursivePartial } from '@tsparticles/engine'
+import { NgxParticlesModule } from '@tsparticles/angular'
+import { loadSlim } from '@tsparticles/slim'
+import { particlesOptionsConfig } from '../../../../assets/animations/particles/particles'
+import { Model3dComponent } from '../../../shared/components/models/model3d/model3d.component'
+import { PasswordModule } from 'primeng/password'
+import { InputTextModule } from 'primeng/inputtext'
+import { FloatLabelModule } from 'primeng/floatlabel'
+import { InputGroupModule } from 'primeng/inputgroup'
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon'
+import { ButtonModule } from 'primeng/button'
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faLock } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { CheckboxComponent } from '../../../shared/components/input/checkbox/checkbox.component'
 
 @Component({
 	selector: 'app-login',
 	standalone: true,
-	imports: [],
+	imports: [
+		NgxParticlesModule,
+		Model3dComponent,
+		PasswordModule,
+		InputTextModule,
+		FloatLabelModule,
+		InputGroupModule,
+		InputGroupAddonModule,
+		FontAwesomeModule,
+		CheckboxComponent,
+		ButtonModule
+	],
 	templateUrl: './login.component.html',
 	styleUrl: './login.component.scss'
 })
-export class LoginComponent {}
+export class LoginComponent {
+	constructor(library: FaIconLibrary) {
+		library.addIcons(faUser, faLock, faFacebook, faGoogle, faGithub) // Register your icons here
+	}
+	public particlesOptions: RecursivePartial<ParticlesOptions> = particlesOptionsConfig
+
+	async particlesInit(engine: Engine): Promise<void> {
+		await loadSlim(engine)
+	}
+}
