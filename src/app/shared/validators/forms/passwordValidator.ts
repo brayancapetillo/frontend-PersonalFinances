1796d87be84f8cdf986a84d9583426a357d7fbb2
Brayan Capetillo <brayancapetoccc@gmail.com>
2024-11-27 23:06:22 -0600

    :sparkles: feat(validator): add custom validation for forms
    This validator introduces custom validation logic for form fields, including:
    - Custom validation for password strength
    - Cross-field validation to ensure password and confirm password fields match

---
+++ src/app/shared/validators/forms/passwordValidator.ts
@@ -0,0 +1,22 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms'
+
+const patternPassword: RegExp = new RegExp('^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&^#_+=~-])[A-Za-z\\d@$!%*?&^#_+=~-]{8,}$')
+
+export const passwordValidator = (control: AbstractControl): ValidationErrors | null => {
+	const value = control.value
+
+	if (!patternPassword.test(value)) {
+		return { passwordValidator: true }
+	}
+
+	return null
+}
+
+export const passwordMatchValidator: ValidatorFn = (
+	formGroupControl: AbstractControl<{ password: string; confirmPassword: string }>
+): ValidationErrors | null => {
+	const password: string = formGroupControl.value.password
+	const confirmPassword: string = formGroupControl.value.confirmPassword
+
+	return password !== confirmPassword ? { passwordMatchValidator: true } : null
+}
