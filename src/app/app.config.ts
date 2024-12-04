62cb6c79828560c0f27958d3de80b474fc087dd3
Brayan Capetillo <brayancapetoccc@gmail.com>
2024-12-04 00:50:40 -0600

    :lipstick: style(transition): add style for transitions in routing of app
    Implement transition styles for smoother page navigation across the app's routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { provideAnimations } from '@angular/platform-browser/animations'
 import { ApplicationConfig } from '@angular/core'
 import { MessageService } from 'primeng/api'
-import { provideRouter } from '@angular/router'
+import { provideRouter, withViewTransitions } from '@angular/router'
 
 import { routes } from './app.routes'
 import { NgxParticlesModule } from '@tsparticles/angular'
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideRouter(routes), provideAnimations(), NgxParticlesModule, MessageService]
+	providers: [provideRouter(routes, withViewTransitions()), provideAnimations(), NgxParticlesModule, MessageService]
 }
