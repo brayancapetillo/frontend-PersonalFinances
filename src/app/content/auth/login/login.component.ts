7f7b9fedaf4f4a312e34f3076b5573e1c7fa01dd
Brayan Capetillo <brayancapetoccc@gmail.com>
2024-12-06 20:32:23 -0600

    :sparkles: feat(component): update `login` component
    This change introduces the following improvements:
    - The login component is now complete, with functionality for login and user registration
    - TSDoc documentation was added to enhance code understanding

--- src/app/content/auth/login/login.component.ts
+++ src/app/content/auth/login/login.component.ts
@@ -1,41 +1,50 @@
 // -Core Angular imports
-import { ReactiveFormsModule } from '@angular/forms'
-import { Component, inject, OnInit } from '@angular/core'
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms'
+import { Component, inject } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { Router } from '@angular/router'
+import { firstValueFrom } from 'rxjs'
 
 // -Particle Engine imports
-import { particlesOptionsConfig } from '../../../../assets/animations/particles/particles'
 import { Engine, ParticlesOptions, RecursivePartial } from '@tsparticles/engine'
+import { particlesOptionsConfig } from '@assets/animations/particles/particles'
 import { NgxParticlesModule } from '@tsparticles/angular'
 import { loadSlim } from '@tsparticles/slim'
-
-// -Custom components and shared modules imports
-import { SwitchModeComponent } from '../../../shared/components/layout/switch-mode/switch-mode.component'
-import { CheckboxComponent } from '../../../shared/components/input/checkbox/checkbox.component'
-import { Model3dComponent } from '../../../shared/components/models/model3d/model3d.component'
-import { passwordValidator } from '../../../shared/validators/forms/passwordValidator'
 
 // -PrimeNG UI components imports
 import { InputTextModule } from 'primeng/inputtext'
 import { PasswordModule } from 'primeng/password'
 import { ToastModule } from 'primeng/toast'
 
-// -Interface's imports
-import { ErrorMessages } from '../../../shared/types/forms/errorControl'
-
 // -FontAwesome imports
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { faFacebook, faGoogle, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faLock, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons'
-import { FormBuilder, Validators } from '@angular/forms'
+
+// -Custom components and shared modules imports
+import { SwitchModeComponent } from '@shared/components/layout/switch-mode/switch-mode.component'
+import { CheckboxComponent } from '@shared/components/input/checkbox/checkbox.component'
+import { Model3dComponent } from '@shared/components/models/model3d/model3d.component'
+
+// -Validators imports
+import { passwordValidator } from '@shared/validators/forms/passwordValidator'
+
+// -Interface's and dto's imports
+import { userSummaryDTO } from '@shared/interfaces/dtos/user/userSummary.dto'
+import { tokenSummary } from '@shared/interfaces/dtos/auth/tokenSumary.dto'
+import { successResponse } from '@shared/interfaces/api/successResponse'
+import { signInDTO } from '@shared/interfaces/dtos/auth/signIn.dto'
+import { signUpDTO } from '@shared/interfaces/dtos/auth/signUp.dto'
 
 // -Service's imports
-import { ToastMessageService } from '../../../core/services/messageService/toast-message.service'
-import { ValidateFormService } from '../../../core/services/forms/validate-form.service'
+import { ErrorHandlerService } from '@core/services/api/http/error/error-handler.service'
+import { ValidateFormService } from '@shared/services/forms/validate-form.service'
+import { UserService } from '@core/services/auth/user/user.service'
+import { AuthService } from '@core/services/api/auth/auth.service'
 
 /**
- * LoginComponent - Login component that allows users to sign in with particle animation
- * and offers integration with FontAwesome icons and PrimeNG modules.
+ * LoginComponent - A component that allows users to sign in or register.
+ * It includes particle animations, FontAwesome integration, and PrimeNG modules.
  */
 @Component({
 	selector: 'app-login',
@@ -56,19 +65,21 @@ import { ValidateFormService } from '../../../core/services/forms/validate-form.
 	templateUrl: './login.component.html',
 	styleUrl: './login.component.scss'
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 	//+============= SERVICES =============+\\
+	private readonly errorHandleService: ErrorHandlerService = inject(ErrorHandlerService)
 	private readonly validateForm: ValidateFormService = inject(ValidateFormService)
-	private readonly messagePNG: ToastMessageService = inject(ToastMessageService)
+	private readonly libraryIcons: FaIconLibrary = inject(FaIconLibrary)
+	private readonly authService: AuthService = inject(AuthService)
 	private readonly formBuilder: FormBuilder = inject(FormBuilder)
-	public libraryIcons: FaIconLibrary = inject(FaIconLibrary)
+	private readonly userService: UserService = inject(UserService)
+	private readonly router: Router = inject(Router)
 
 	//+============== GLOBAL ==============+\\
 	public particlesOptions: RecursivePartial<ParticlesOptions> = particlesOptionsConfig
 	public isLogin = false
 
 	//+============== FORMS ===============+\\
-	public errorMessages: ErrorMessages = this.validateForm.errorMessage
 	public formLogin = this.formBuilder.nonNullable.group({
 		email: ['', [Validators.required, Validators.email]],
 		password: ['', [Validators.required, passwordValidator]]
@@ -81,18 +92,9 @@ export class LoginComponent implements OnInit {
 
 	/**
 	 * Constructor for the component that registers necessary icons in the FontAwesome library.
-	 *
-	 * @param library - Instance of FaIconLibrary to add icons used in the component.
 	 */
 	constructor() {
 		this.libraryIcons.addIcons(faUser, faLock, faFacebook, faGoogle, faGithub, faEnvelope)
-	}
-
-	/**
-	 * Angular lifecycle method that runs after the component is initialized.
-	 */
-	ngOnInit(): void {
-		this.isLogin
 	}
 
 	/**
@@ -107,35 +109,99 @@ export class LoginComponent implements OnInit {
 
 	/**
 	 * Toggles the `isLogin` value to switch between login and registration forms.
+	 * @public
+	 * @returns {void}
 	 */
 	public changeForm(): void {
 		this.isLogin = !this.isLogin
 	}
 
 	//&============ FUNCTIONS FOR LOGIN ============&\\
-	public login(): void {
+	/**
+	 * Handles the user login process.
+	 * Validates the form and registers the user if valid.
+	 * @public
+	 * @returns {Promise<void>} A promise that resolves once the login process completes.
+	 */
+	public async login(): Promise<void> {
 		if (this.formLogin.invalid) {
 			this.validateLoginForm()
 			return
 		}
+
+		const { email, password } = this.formLogin.controls
+		const loginData: signInDTO = {
+			email: email.value,
+			password: password.value
+		}
+
+		try {
+			const resSignIn: successResponse<tokenSummary> = await firstValueFrom(this.authService.signIn(loginData))
+
+			if (resSignIn.success) {
+				const tokens: tokenSummary = resSignIn.data
+				await this.userService.setUserByToken(tokens)
+
+				this.router.navigate(['/dashboard'])
+			}
+		} catch (error: unknown) {
+			this.errorHandleService.errorHandleHttp('warn', error, 'signIn')
+		}
 	}
 
+	/**
+	 * Validates the controls of the login form.
+	 * @private
+	 * @returns {void}
+	 */
 	private validateLoginForm(): void {
-		this.validateForm.notifyErrorFormControl('error', this.formLogin.controls.email, this.errorMessages.email)
-		this.validateForm.notifyErrorFormControl('error', this.formLogin.controls.password, this.errorMessages.password)
+		this.validateForm.notifyErrorFormControl('error', this.formLogin.controls.email, 'email')
+		this.validateForm.notifyErrorFormControl('error', this.formLogin.controls.password, 'password')
 	}
 
 	//&============ FUNCTIONS FOR REGISTER ============&\\
-	public register(): void {
+	/**
+	 * Handles the user registration process.
+	 * Validates the form and registers the user if valid.
+	 * @public
+	 * @returns {Promise<void>} A promise that resolves once the registration process completes.
+	 */
+	public async register(): Promise<void> {
 		if (this.formRegister.invalid) {
 			this.validateRegisterForm()
 			return
 		}
+
+		const { userName, email, password } = this.formRegister.controls
+		const registerData: signUpDTO = {
+			name: userName.value,
+			email: email.value,
+			password: password.value,
+			birthday: null,
+			idLenguage: 1,
+			idSex: 1,
+			lastName: null,
+			phone: null
+		}
+
+		try {
+			const resSignUp: successResponse<userSummaryDTO> = await firstValueFrom(this.authService.signUp(registerData))
+
+			if (resSignUp.success) {
+				this.router.navigate(['auth/verifyEmail'], { queryParams: { email: email.value } })
+			}
+		} catch (error: unknown) {
+			this.errorHandleService.errorHandleHttp('warn', error, 'signUp')
+		}
 	}
 
-	public validateRegisterForm(): void {
-		this.validateForm.notifyErrorFormControl('error', this.formRegister.controls.userName, this.errorMessages.userName)
-		this.validateForm.notifyErrorFormControl('error', this.formRegister.controls.email, this.errorMessages.email)
-		this.validateForm.notifyErrorFormControl('error', this.formRegister.controls.password, this.errorMessages.password)
+	/**
+	 * Validates the controls of the registration form.
+	 * @private
+	 */
+	private validateRegisterForm(): void {
+		this.validateForm.notifyErrorFormControl('error', this.formRegister.controls.userName, 'userName')
+		this.validateForm.notifyErrorFormControl('error', this.formRegister.controls.email, 'email')
+		this.validateForm.notifyErrorFormControl('error', this.formRegister.controls.password, 'password')
 	}
 }
