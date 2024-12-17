f5a9195dab1341d046bf9fb0bc0d33da731cbb59
Brayan Capetillo <brayancapetoccc@gmail.com>
2024-12-16 23:51:00 -0600

    :sparkles: feat(constants): add theme color constants

---
+++ src/app/shared/constants/colors/themeColors.ts
@@ -0,0 +1,23 @@
+export const themeColors: string[] = [
+	'#fefaec',
+	'#faf2cb',
+	'#f7e38a',
+	'#f1d46c',
+	'#eec55b',
+	'#be9b49',
+	'#a28240',
+	'#896e36',
+	'#66502d',
+	'#58442b',
+	'#261f0e'
+]
+
+export const myTextColor: string = '#232323'
+export const myTextColorSecondary: string = '#989a9e'
+export const myHighlightTextColor: string = '#eec55b'
+
+export const myTextColorDark: string = '#ffffff'
+export const myTextColorSecondaryDark: string = '#5d657b'
+
+export const mySurfaceGround: string = '#e3e9f7'
+export const mySurfaceGroundDark: string = '#161616'
